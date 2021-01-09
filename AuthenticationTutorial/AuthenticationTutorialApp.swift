5685e22c419031cc8cea3c20bf0898a5af817047
chefeleUNH <chefele@newhaven.edu>
2021-01-09 14:46:14 -0500

    Initial Commit

---
+++ AuthenticationTutorial/AuthenticationTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthenticationTutorialApp.swift
+//  AuthenticationTutorial
+//
+//  Created by Charles Hefele on 1/9/21.
+//
+
+import SwiftUI
+
+@main
+struct AuthenticationTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
