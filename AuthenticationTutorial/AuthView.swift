14f63350cddb40bdce2f21613207b64b95de2d47
chefeleUNH <chefele@newhaven.edu>
2021-01-12 21:55:35 -0500

    completed tutorial from DesignIntoCode - https://youtu.be/FhLEwqyVSjE

---
+++ AuthenticationTutorial/AuthView.swift
@@ -0,0 +1,158 @@
+//
+//  AuthView.swift
+//  AuthenticationTutorial
+//
+//  Created by Charles Hefele on 1/9/21.
+//
+
+import SwiftUI
+
+struct AuthView: View {
+    var body: some View {
+        NavigationView {
+            SignInView()
+        }
+    }
+}
+
+struct SignInView: View {
+    @State var email: String = ""
+    @State var password: String = ""
+    @State var error: String = ""
+    @EnvironmentObject var session: FirebaseSession
+    
+    func signIn() {
+        session.signIn(email: email, password: password) { (result, error) in
+            if let error = error {
+                self.error = error.localizedDescription
+            } else {
+                self.email = ""
+                self.password = ""
+            }
+        }
+    }
+    
+    var body: some View {
+        VStack {
+            Text("Welcome Back!")
+                .font(.system(size: 32, weight: .heavy))
+            
+            Text("Sign in to continue")
+                .font(.system(size: 18, weight: .medium))
+                .foregroundColor(.gray)
+            
+            VStack(spacing: 18) {
+                TextField("Email address", text: $email)
+                    .font(.system(size: 14))
+                    .padding(12)
+                    .background(RoundedRectangle(cornerRadius: 5).strokeBorder(Color.primary, lineWidth: 1))
+                
+                SecureField("Password", text: $password)
+                    .font(.system(size: 14))
+                    .padding(12)
+                    .background(RoundedRectangle(cornerRadius: 5).strokeBorder(Color.primary, lineWidth: 1))
+            }
+            .padding(.vertical, 64)
+            
+            Button(action: signIn) {
+                Text("Sign in")
+                    .frame(minWidth: 0, maxWidth: .infinity)
+                    .frame(height: 50)
+                    .foregroundColor(.white)
+                    .font(.system(size: 14, weight: .bold))
+                    .background(LinearGradient(gradient: Gradient(colors: [Color.primary, Color.secondary]), startPoint:  .leading, endPoint: .trailing))
+                    .cornerRadius(5)
+            }
+            
+            if (error != "") {
+                Text(error)
+                    .font(.system(size: 14, weight: .semibold))
+                    .foregroundColor(.red)
+                    .padding()
+            }
+            
+            Spacer()
+            
+            NavigationLink(destination: SignUpView()) {
+                HStack {
+                    Text("I'm a new user.")
+                        .font(.system(size: 14, weight: .light))
+                        .foregroundColor(.primary)
+                    
+                    Text("Create an account")
+                        .font(.system(size: 14, weight: .semibold))
+                        .foregroundColor(.secondary)
+                }
+            }
+        }
+        .padding(.horizontal, 32)
+    }
+}
+
+struct SignUpView: View {
+    @State var email: String = ""
+    @State var password: String = ""
+    @State var error: String = ""
+    @EnvironmentObject var session: FirebaseSession
+    
+    func signUp() {
+        session.signUp(email: email, password: password) { (result, error) in
+            if let error = error {
+                self.error = error.localizedDescription
+            } else {
+                self.email = ""
+                self.password = ""
+            }
+        }
+    }
+    
+    var body: some View {
+        VStack {
+            Text("Create Account")
+                .font(.system(size: 32, weight: .heavy))
+            
+            Text("Sign up to get started")
+                .font(.system(size: 18, weight: .medium))
+                .foregroundColor(.gray)
+            
+            VStack(spacing: 18) {
+                TextField("Email address", text: $email)
+                    .font(.system(size: 14))
+                    .padding(12)
+                    .background(RoundedRectangle(cornerRadius: 5).strokeBorder(Color.primary, lineWidth: 1))
+                
+                SecureField("Password", text: $password)
+                    .font(.system(size: 14))
+                    .padding(12)
+                .background(RoundedRectangle(cornerRadius: 5).strokeBorder(Color.primary, lineWidth: 1))
+            }.padding(.vertical, 64)
+            
+            Button(action: signUp) {
+                Text("Create Account")
+                    .frame(minWidth: /*@START_MENU_TOKEN@*/0/*@END_MENU_TOKEN@*/, maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)
+                    .frame(height: 50)
+                    .foregroundColor(.white)
+                    .font(.system(size: 14, weight: .bold))
+                    .background(LinearGradient(gradient: Gradient(colors: [Color.primary, Color.secondary]),
+                                               startPoint: .leading, endPoint: .trailing))
+                    .cornerRadius(5)
+            }
+            
+            if (error != "") {
+                Text(error)
+                    .font(.system(size: 14, weight: .semibold))
+                    .foregroundColor(.red)
+                    .padding()
+            }
+            
+            Spacer()
+            
+        }.padding(.horizontal, 32)
+    }
+}
+
+struct AuthView_Previews: PreviewProvider {
+    static var previews: some View {
+        AuthView().environmentObject(FirebaseSession())
+    }
+}
--- AuthenticationTutorial/ContentView.swift
+++ AuthenticationTutorial/ContentView.swift
@@ -8,14 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    @EnvironmentObject var session: FirebaseSession
+    
+    func getUser() {
+        session.listen()
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Group {
+            if (session.user != nil) {
+                Text("Welcome back \(session.user?.email ?? "undefined")!")
+                Button(action: session.signOut) {
+                    Text("Sign Out")
+                }
+            } else {
+                AuthView()
+            }
+        }.onAppear(perform: getUser)
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().environmentObject(FirebaseSession())
     }
 }
